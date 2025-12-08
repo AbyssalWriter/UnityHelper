5e8e19fbd672a02876bf5a918eabdf85bbae5b2f
AbyssalWriter <abyssalwriter@gmail.com>
2025-12-08 06:02:35 -0500

    Add font size on debug

--- Assets/UnityHelper/Scripts/Helper/Debug/DebugHelper.cs
+++ Assets/UnityHelper/Scripts/Helper/Debug/DebugHelper.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Helper.Debug
 {
-    public class DebugHelper: Shared.Singleton<DebugHelper>
+    public class DebugHelper : Shared.Singleton<DebugHelper>
     {
-        [SerializeField] private Rect rectBox = new(10,0, 750, 20);
+        [SerializeField] private Rect rectBox = new(10, 0, 750, 20);
         [SerializeField] private float logOffset = 10;
         [SerializeField] private float clearLogsTimerInSeconds = 5f;
+        [SerializeField] private int fontSize = 20;
+
+        private GUIStyle labelStyle;
 
         private Dictionary<string, object> _logs = new();
         private float _nextClease;
 
+
+
         public void Log(string key, object value)
         {
-            if(_logs.TryAdd(key,value)) return;
-            
+            if (_logs.TryAdd(key, value)) return;
+
             _logs[key] = value;
         }
 
@@ -33,17 +36,22 @@ namespace Helper.Debug
 
         private void OnGUI()
         {
+            labelStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontSize = fontSize
+            };
+
             var clone = _logs.ToDictionary(entry => entry.Key,
                 entry => entry.Value);
 
             var drawingRect = new Rect(rectBox);
             GUILayout.BeginArea(new Rect(0, 0, rectBox.width, Screen.height));
-        
+
             foreach (var keyValuePair in clone)
             {
                 drawingRect.y += drawingRect.height + logOffset;
-                GUI.Label(drawingRect, keyValuePair.Key +" "+ keyValuePair.Value);
-  
+                GUI.Label(drawingRect, keyValuePair.Key + " " + keyValuePair.Value, labelStyle);
+
             }
             GUILayout.EndArea();
         }
